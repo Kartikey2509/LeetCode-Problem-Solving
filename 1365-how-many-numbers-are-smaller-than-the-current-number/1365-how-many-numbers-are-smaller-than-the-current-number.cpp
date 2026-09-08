23397e585ac103ba24bef35c22fd806fcdd85e2b
Kartikey Upadhyay <dkkartikey2509@gmail.com>
2026-09-08 14:26:58 +0530

    Time: 0 ms (100%), Space: 14.5 MB (19.88%) - LeetHub - Time: O(n log n), Space: O(n)

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> sorted = nums;
+
+        // Sort the copy
+        sort(sorted.begin(), sorted.end());
+
         vector<int> result;
-        
-        for(int i=0 ; i<nums.size(); i++){
-            int count=0;
-            for(int j=0 ; j<nums.size();j++){
-                if(nums[j]<nums[i]){
-                    count++;
+
+        for (int num : nums) {
+            int low = 0;
+            int high = sorted.size() - 1;
+            int pos = sorted.size();
+
+            //  lower_bound
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+
+                if (sorted[mid] >= num) {
+                    pos = mid;
+                    high = mid - 1;
+                } 
+                else {
+                    low = mid + 1;
                 }
-                
             }
-            result.push_back(count);
+
+            result.push_back(pos);
         }
-    return result;
+
+        return result;
     }
 };
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
