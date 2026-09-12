8c275a9a4c437bf1aef853356ae2cbd8963e605b
Kartikey Upadhyay <dkkartikey2509@gmail.com>
2026-09-12 21:06:22 +0530

    Time: 0 ms (100%), Space: 10.1 MB (86.3%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    int value(char ch){
+        switch(ch){
+            case 'I' : return 1;
+            case 'V' : return 5;
+            case 'X' : return 10;
+            case 'L' : return 50;
+            case 'C' : return 100;
+            case 'D' : return 500;
+            case 'M' : return 1000;
+        }
+        return 0;
+    }
+    
+    int romanToInt(string s) {
+        int ans=0;
+        for(int i=0 ; i<s.size() ; i++){
+            if(i+1<s.size() && value(s[i])<value(s[i+1])){
+                ans-=value(s[i]);
+            }else{
+                ans+=value(s[i]);
+            }
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
